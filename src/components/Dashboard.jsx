b13742fb488994f9682025acb364419da7364327
shahzebkhan7786 <53787072+shahzebkhan7786@users.noreply.github.com>
2026-03-18 17:28:19 +0530

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,1 +1,14 @@
+export default function Dashboard() {
+  return (
+    <div className="container">
+      <h2>Supplier Dashboard</h2>
 
+      <div className="card">
+        <p>Total Suppliers: 2</p>
+        <p>High Risk: 1 🔴</p>
+        <p>Expired Certs: 1 ⚠️</p>
+        <p>Open Queries: 2</p>
+      </div>
+    </div>
+  );
+}
