f7ac8d9ecf5e532dc829fa8e96d552da8e3424bb
shahzebkhan7786 <53787072+shahzebkhan7786@users.noreply.github.com>
2026-03-18 17:11:06 +0530

    Create QueryList.jsx

---
+++ src/components/QueryList.jsx
@@ -0,0 +1,12 @@
+export default function QueryList() {
+  return (
+    <div className="container">
+      <h2>Query Tracking</h2>
+
+      <div className="card">
+        <p>#1 - ABC Foods - Pending 🔴</p>
+        <p>#2 - Fresh Farms - Resolved 🟢</p>
+      </div>
+    </div>
+  );
+}
