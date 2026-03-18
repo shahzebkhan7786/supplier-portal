60cb40f46a5177e74260624d86f009804f6a6f09
shahzebkhan7786 <53787072+shahzebkhan7786@users.noreply.github.com>
2026-03-18 17:12:42 +0530

    Create QueryDetail.jsx

---
+++ src/components/QueryDetail.jsx
@@ -0,0 +1,14 @@
+export default function QueryDetail() {
+  return (
+    <div className="container">
+      <h2>Query Detail</h2>
+
+      <div className="card">
+        <p><b>QA:</b> Upload certificate</p>
+        <p><b>Supplier:</b> Will upload soon</p>
+      </div>
+
+      <input placeholder="Type message..." />
+    </div>
+  );
+}
